0f25215cd8c975b0239e2a97ac220ebef16955f0
carloxavz <carlosantonioav@ufps.edu.co>
2025-04-25 06:25:16 -0500

    no sé

--- biblioteca_project/urls.py
+++ biblioteca_project/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from biblioteca import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.inicio, name='inicio'),
 ]
+
+
